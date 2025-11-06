ef19e647db4ad3992cdcfccf9f0ae84fcea2ba28
Sung hun kwag <speedkjr13@naver.com>
2025-11-06 21:16:35 +0900

    Update __init__.py to v3.1 imports

--- dhc_ssm/core/__init__.py
+++ dhc_ssm/core/__init__.py
@@ -1,15 +1,10 @@
-"""Core architecture components for DHC-SSM."""
+"""Core architecture components for DHC-SSM v3.1"""
 
-from dhc_ssm.core.spatial_encoder import SpatialEncoder
-from dhc_ssm.core.temporal_processor import TemporalProcessor
-from dhc_ssm.core.strategic_reasoner import StrategicReasoner
-from dhc_ssm.core.learning_engine import LearningEngine
-from dhc_ssm.core.model import DHCSSMModel
+from dhc_ssm.core.model import DHCSSMModel, DHCSSMConfig, SpatialEncoder, TemporalSSM
 
 __all__ = [
-    "SpatialEncoder",
-    "TemporalProcessor",
-    "StrategicReasoner",
-    "LearningEngine",
     "DHCSSMModel",
+    "DHCSSMConfig",
+    "SpatialEncoder",
+    "TemporalSSM",
 ]
